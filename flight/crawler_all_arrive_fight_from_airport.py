bc5e682f458fb5a3d254775400c4b8517f364252
Lily_Chou <98002855+hellowhaaa@users.noreply.github.com>
2024-04-15 18:59:40 +0800

    test: test airflow

--- flight/crawler_all_arrive_fight_from_airport.py
+++ flight/crawler_all_arrive_fight_from_airport.py
@@ -1,10 +1,4 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import timedelta, datetime
-import pendulum
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,60 +8,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import TimeoutException
 from dotenv import load_dotenv
 import os
-from airflow.utils.dates import days_ago
+load_dotenv()
 
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# Setup Chrome options
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")  # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Set path to chromedriver as per your configuration
+webdriver_service = Service(ChromeDriverManager().install())
+
+# Choose Chrome Browser
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
 def crawl_data():
     try:
-        options = webdriver.ChromeOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        
-        # Set command timeout to 60 seconds
-        capabilities = options.to_capabilities()
-        driver = webdriver.Remote(
-            command_executor='http://remote_chromedriver:4444/wd/hub',
-            keep_alive=True,
-            options=options # Increase the timeout to 60 seconds
-        )
-        
         url = 'https://www.taoyuan-airport.com/flight_arrival?k=&time=all'
         driver.get(url)
-    # try:
-        # Setup Chrome options
-        # chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("--headless")  # Ensure GUI is off
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-        # url = 'https://www.taoyuan-airport.com/flight_arrival?k=&time=all'
-        # Set path to chromedriver as per your configuration
-        # webdriver_service = Service(ChromeDriverManager().install())
-        # chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-
-        # Connect to the remote ChromeDriver through Selenium
-    #     try:
-    #         # remote_webdriver = 'remote_chromedriver'
-    #         options = webdriver.ChromeOptions()
-    #         options.add_argument('--ignore-ssl-errors=yes')
-    #         options.add_argument('--ignore-certificate-errors')
-    #         driver = webdriver.Remote(
-    #             command_executor='http://remote_chromedriver:4444/wd/hub',
-    #             options=options)
-    #             # Scraping part
-    #         url = 'https://www.taoyuan-airport.com/flight_arrival?k=&time=all'
-    #         driver.get(url)
-    #     except Exception as e:
-    #         print(e)
-    #     # Choose Chrome Browser
-    #     # driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
-        
-    #     # with webdriver.Remote(f'{remote_webdriver}:4444/wd/hub', options=options) as driver:
-    #     # driver.get(url)
         for i in range(2,500):
             print(i)
         # Use the correct method to find the element
@@ -83,6 +42,7 @@ def crawl_data():
             terminal = f'//*[@id="print"]/ul[2]/li[{i}]/div[4]'
             gate = f'//*[@id="print"]/ul[2]/li[{i}]/div[5]'
             status = f'//*[@id="print"]/ul[2]/li[{i}]/div[7]/p'
+            # Wait for the required element to load on the page
             # ? ------ airline ------
             try:  
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, airline)))
@@ -166,43 +126,18 @@ def crawl_data():
                 'gate': gate_element,
                 'status': status_element    
             }
-            # load_dotenv()
-            # uri = os.getenv("MONGODB_URI")
-            # conn = MongoClient(uri)
-            # try:
-            #     conn.admin.command('ping')
-            #     print("Pinged your deployment. You successfully connected to MongoDB!")
-            # except Exception as e:
-            #     print(e)
-            # db = conn['flying_high']
-            # collection = db['flight_arrive']
-            # collection.insert_one(flight_dic)
-            try:
-                insert_to_mongo(flight_dic)
-            except Exception as e:
-                print(e)
-    except Exception as e:
-        print(f"Error during web scraping: {e}")
+            collection = insert_mongodb_atlas()
+            collection.insert_one(flight_dic)
 
-    #         print('--------------------')
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
+            print('--------------------')
+    except Exception as e:
+        print(f"An error occurred: {e}")
             
-    # finally:
-    #     # Close the browser
-        # driver.quit()
-def insert_to_mongo(data):
-    # HCadEw7bWkMlybDF
-    DATABASE_URL = "mongodb+srv://root:HCadEw7bWkMlybDF@cluster0.ddhtgvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    # DATABASE_URL = "mongodb+srv://admin:HCadEw7bWkMlybDF@cluster0.ibhiiti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    client = MongoClient(DATABASE_URL)
-    collection = client['flying_high']['flight_arrive']
-    if data:
-        collection.insert_one(data)
-    client.close()
-    
+    finally:
+        # Close the browser
+        driver.quit()
+        
 def insert_mongodb_atlas():
-    load_dotenv()
     uri = os.getenv("MONGODB_URI")
     conn = MongoClient(uri)
     try:
@@ -210,52 +145,18 @@ def insert_mongodb_atlas():
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
-    db = conn['flying_high']
-    collection = db['flight_arrive']
-    if_database_exist()
-    return collection
 
-def if_database_exist():
-    uri = os.getenv("MONGODB_URI")
-    conn = MongoClient(uri)
+    # Create a MongoClient instance
+    db = conn['flying_high']
+    # Access a collection (similar to a table in relational databases)
+    collection = db['flight_arrive']
+
     mongo_dblist = conn.list_database_names()
     if "flying_high" in mongo_dblist:
         print("flying_high database 已存在！")
     else:
         print('flying_high database 不存在')
-
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False, 
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
-
-with DAG(
-    dag_id="arrive_flight",
-    schedule="*/30 * * * *",
-    start_date=pendulum.datetime(2024, 4, 4, tz="UTC"),
-    default_args=default_args,
-    catchup=False, # 不會去執行以前的任務
-    max_active_runs=1,
-    tags=['flight'],
-) as dag:
-    task_start = EmptyOperator(
-    task_id="task_start",
-    dag=dag
-    )
-    task_end = EmptyOperator(
-    task_id="task_end",
-    dag=dag
-    )
     
-    task_arrive_flight = PythonOperator(
-        task_id = "insert_arrive_flight_mongodb",
-        python_callable=crawl_data,
-        dag = dag  
-    )
+    return collection
     
-(task_start >> task_arrive_flight >> task_end)
+crawl_data()        
