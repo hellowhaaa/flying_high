e2ddb3a17ec09da1a5f253f7139ea751c7472a38
Lily_Chou <98002855+hellowhaaa@users.noreply.github.com>
2024-04-15 15:16:34 +0800

    feat: revise return json format of arriving time

--- crawler_all_arrive_fight_from_airport.py
+++ crawler_all_arrive_fight_from_airport.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import TimeoutException
 from dotenv import load_dotenv
+import datetime
 import os
 load_dotenv()
 
@@ -30,6 +31,7 @@ def crawl_data():
         for i in range(2,500):
             print(i)
         # Use the correct method to find the element
+            taiwan_title_time = '//*[@id="print"]/p[2]'
             scheduled_arrive_time = f'//*[@id="print"]/ul[2]/li[{i}]/div[1]/span[2]'
             actual_arrive_time = f'//*[@id="print"]/ul[2]/li[{i}]/div[8]/span[2]'
             # next_sch_path = '//*[@id="print"]/ul[2]/li[3]/div[1]/span[2]'
@@ -43,6 +45,14 @@ def crawl_data():
             gate = f'//*[@id="print"]/ul[2]/li[{i}]/div[5]'
             status = f'//*[@id="print"]/ul[2]/li[{i}]/div[7]/p'
             # Wait for the required element to load on the page
+            # ? ------ taiwan_title_time ------
+            try:  
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, taiwan_title_time)))
+                taiwan_title_time_element = driver.find_element(By.XPATH, taiwan_title_time).text.strip()
+                print(taiwan_title_time_element)
+            except TimeoutException:
+                taiwan_title_time_element = "Not Found"
+                print(f"airlines: {taiwan_title_time_element}") 
             # ? ------ airline ------
             try:  
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, airline)))
@@ -117,17 +127,33 @@ def crawl_data():
             except TimeoutException:
                 status_element = "Not Found"
                 print(f"Status: {status_element}")
-            flight_dic = {
-                "scheduled_depart_time": scheduled_arrive_time_element,
-                "actual_depart_time": actual_arrive_time_element,
-                "destination": destination_element,           
-                'airline': alphabet_ls,
-                'terminal': terminal_element,
-                'gate': gate_element,
-                'status': status_element    
-            }
             collection = insert_mongodb_atlas()
-            collection.insert_one(flight_dic)
+            result = collection.update_many(
+                {
+                    "taiwan_title_time": taiwan_title_time_element,  # 符合的日期時間
+                    "airline": alphabet_ls  # 符合的航空公司
+                },
+                {
+                    "$set": {
+                        "scheduled_depart_time": scheduled_arrive_time_element,
+                        "actual_depart_time": actual_arrive_time_element,
+                        "destination": destination_element,
+                        "airline": alphabet_ls,
+                        "terminal": terminal_element,
+                        "gate": gate_element,
+                        "status": status_element,
+                        "updated_at": datetime.datetime.utcnow()
+                    },
+                    "$setOnInsert": {
+                        "created_at": datetime.datetime.utcnow()  # 只有在首次創建時 insert
+                    }
+                },
+                upsert=True  # 如果沒有找到 match 'taiwan_title_time', 'airline'的 document，就 insert 一個新的
+            )
+            print(f'Matched count: {result.matched_count}')
+            print(f'Modified count: {result.modified_count}')
+            if result.upserted_id:
+                print(f'Upserted ID: {result.upserted_id}')  # 新建 document 的 ID
 
             print('--------------------')
     except Exception as e:
