98840f3d7e66a51edbc979db6cd6e198063cb095
Lily_Chou <98002855+hellowhaaa@users.noreply.github.com>
2024-06-08 14:31:56 +0800

    refactor: clean back up to s3 function and create maon_steps

--- flight/dags/depart_flight.py
+++ flight/dags/depart_flight.py
@@ -28,7 +28,14 @@ FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 
-def crawl_data():
+def main_steps():
+    for i in range(2, 310):
+        crawled_data = crawl_data(i)
+        insert_mongodb_atlas(crawled_data)
+        # back_up_to_s3(crawled_data)
+    
+
+def crawl_data(i):
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-ssl-errors=yes')
     options.add_argument('--ignore-certificate-errors')
@@ -36,116 +43,72 @@ def crawl_data():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     
-    # Set command timeout to 60 seconds
     capabilities = options.to_capabilities()
     driver = webdriver.Remote(
         command_executor='http://remote_chromedriver:4444/wd/hub',
         keep_alive=True,
-        options=options # Increase the timeout to 60 seconds
+        options=options 
     )
 
     url = 'https://www.taoyuan-airport.com/flight_depart?k=&time=all'
     driver.get(url)
 
     try:
-        for i in range(2, 310):
-            # taiwan_title_time
-            taiwan_title_time = '//*[@id="print"]/p[2]'
-            taiwan_title_time_element = get_taiwan_title_time(taiwan_title_time, driver)
+    
+        # taiwan_title_time
+        taiwan_title_time = '//*[@id="print"]/p[2]'
+        taiwan_title_time_element = get_taiwan_title_time(taiwan_title_time, driver)
 
-            # scheduled_depart_time
-            scheduled_depart_time = f'//*[@id="print"]/ul[2]/li[{i}]/div[1]/span[2]'
-            scheduled_depart_time_element = get_scheduled_depart_time(scheduled_depart_time, driver)
+        # scheduled_depart_time
+        scheduled_depart_time = f'//*[@id="print"]/ul[2]/li[{i}]/div[1]/span[2]'
+        scheduled_depart_time_element = get_scheduled_depart_time(scheduled_depart_time, driver)
 
-            # actual_depart_time
-            actual_depart_time = f'//*[@id="print"]/ul[2]/li[{i}]/div[8]/span[2]'
-            actual_depart_time_element = get_actual_depart_time(actual_depart_time, driver)
+        # actual_depart_time
+        actual_depart_time = f'//*[@id="print"]/ul[2]/li[{i}]/div[8]/span[2]'
+        actual_depart_time_element = get_actual_depart_time(actual_depart_time, driver)
 
-            # destination
-            destination = f'//*[@id="print"]/ul[2]/li[{i}]/div[2]/p[2]'
-            destination_element = get_destination(destination, driver)
+        # destination
+        destination = f'//*[@id="print"]/ul[2]/li[{i}]/div[2]/p[2]'
+        destination_element = get_destination(destination, driver)
 
-            # airlines
-            airline = f'//*[@id="print"]/ul[2]/li[{i}]/div[3]'
-            alphabet_ls = get_airlines(airline, driver)
+        # airlines
+        airline = f'//*[@id="print"]/ul[2]/li[{i}]/div[3]'
+        alphabet_ls = get_airlines(airline, driver)
 
-            # terminal
-            terminal = f'//*[@id="print"]/ul[2]/li[{i}]/div[4]'
-            terminal_element = get_terminal(terminal, driver)
+        # terminal
+        terminal = f'//*[@id="print"]/ul[2]/li[{i}]/div[4]'
+        terminal_element = get_terminal(terminal, driver)
 
-            # gate
-            gate = f'//*[@id="print"]/ul[2]/li[{i}]/div[5]'
-            gate_element = get_gate(gate, driver)
+        # gate
+        gate = f'//*[@id="print"]/ul[2]/li[{i}]/div[5]'
+        gate_element = get_gate(gate, driver)
 
-            # status
-            status = f'//*[@id="print"]/ul[2]/li[{i}]/div[7]/p'
-            status_element = get_status(status, driver)
+        # status
+        status = f'//*[@id="print"]/ul[2]/li[{i}]/div[7]/p'
+        status_element = get_status(status, driver)
 
-            # time_difference
-            time_diff_minute = calculate_time_diff(actual_depart_time_element, scheduled_depart_time_element)
-            collection = insert_mongodb_atlas()
-
-            try:
-                result = collection.update_many(
-                    {
-                        "taiwan_title_time": taiwan_title_time_element,  # 符合的日期時間
-                        "airline": alphabet_ls  # 符合的航空公司
-                    },
-                    {
-                        "$set": {
-                            "scheduled_depart_time": scheduled_depart_time_element,
-                            "actual_depart_time": actual_depart_time_element,
-                            "destination": destination_element,
-                            "airline": alphabet_ls,
-                            "terminal": terminal_element,
-                            "gate": gate_element,
-                            "status": status_element,
-                            "time_difference": time_diff_minute,
-                            "updated_at": datetime.datetime.utcnow()
-                        },
-                        "$setOnInsert": {
-                            "created_at": datetime.datetime.utcnow()  # 只有在首次創建時 insert
-                        }
-                    },
-                    upsert=True  # 如果沒有找到 match 'taiwan_title_time', 'airline'的 document，就 insert 一個新的
-                )
-                logging.info(f"Matched count: {result.matched_count}")
-                logging.info(f"Modified count: {result.modified_count}")
-                if result.upserted_id:
-                    logging.info(f'Upserted ID: {result.upserted_id}')  # 新建 document 的 ID
-            except Exception as e:
-                logging.error(f"An exception occurred: {str(e)}", exc_info=True)
-
-            print('--------------------')
-            
-            # Backup to S3
-            # taiwan_time_split = taiwan_title_time_element.split("機")[1].split(" (")[0].strip().split("/")
-            # taiwan_time_file = f"depart/{taiwan_time_split[0]}-{taiwan_time_split[1]}-{taiwan_time_split[2]}"
-            # back_up_data = {
-            #     "taiwan_title_time": taiwan_title_time_element,
-            #     "airline": alphabet_ls,
-            #     "scheduled_depart_time": scheduled_depart_time_element,
-            #     "actual_depart_time": actual_depart_time_element,
-            #     "destination": destination_element,
-            #     "terminal": terminal_element,
-            #     "gate": gate_element,
-            #     "status": status_element,
-            #     "time_difference": time_diff_minute,
-            # }
-            
-            # try:
-            #     airline_code = ""
-            #     for flight in alphabet_ls:
-            #         airline_code += flight['airline_code']
-            #         airline_code += "_"
-            #     key = f"{taiwan_time_file}_{airline_code}.json"
-            #     backup_to_s3(key, back_up_data)
-            # except Exception as e:
-            #     logging.error(f"An exception occurred: {str(e)}", exc_info=True)
+        # time_difference
+        time_diff_minute = calculate_time_diff(actual_depart_time_element, scheduled_depart_time_element)
+        
+        json_format = {
+                    "taiwan_title_time": taiwan_title_time_element,  
+                    "airline": alphabet_ls,
+                    "scheduled_depart_time": scheduled_depart_time_element,
+                    "actual_depart_time": actual_depart_time_element,
+                    "destination": destination_element,
+                    "terminal": terminal_element,
+                    "gate": gate_element,
+                    "status": status_element,
+                    "time_difference": time_diff_minute
+                }
+        return json_format
+        
+        
     except Exception as e:
         logging.error(f"An exception occurred: {str(e)}", exc_info=True)        
     finally:
         driver.quit()
+
 
 def get_taiwan_title_time(taiwan_title_time, driver):
     try:  
@@ -195,6 +158,7 @@ def get_scheduled_depart_time(scheduled_depart_time, driver):
     except TimeoutException as e:
         logging.error(f"An exception occurred: {str(e)}. scheduled_depart_time not found", exc_info=True) 
 
+
 def get_destination(destination, driver):
     try:
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, destination)))
@@ -203,6 +167,7 @@ def get_destination(destination, driver):
         return destination_element
     except TimeoutException as e:
         logging.error(f"An exception occurred: {str(e)}. destination not found", exc_info=True) 
+
 
 def get_terminal(terminal, driver):
     try:  
@@ -234,34 +199,71 @@ def get_status(status, driver):
         logging.error(f"An exception occurred: {str(e)}. status not found", exc_info=True)
     
     
-    
-def insert_mongodb_atlas():
+def insert_mongodb_atlas(crawled_data):
     load_dotenv()
     uri = os.getenv("MONGODB_URI_FLY")
     client = MongoClient(uri)
     db = client['flying_high']
     collection = db['flight_depart2']
-    return collection
-
-def backup_to_s3(key, back_up_data):
     try:
-        load_dotenv()
-        bucket_name = os.getenv('BUCKET_NAME_flying_high')
-        aws_access_key = os.getenv('AWS_ACCESS_KEY')
-        aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        bucket_region = os.getenv('S3_BUCKET_REGION')
-
-
-        # S3 客戶端配置
-        s3_client = boto3.client(
-            's3',
-            region_name=bucket_region,
-            aws_access_key_id=aws_access_key,
-            aws_secret_access_key=aws_secret_access_key
+        result = collection.update_many(
+            {
+                "taiwan_title_time": crawled_data['taiwan_title_time'],  
+                "airline": crawled_data['airline'] 
+            },
+            {
+                "$set": {
+                    "scheduled_depart_time": crawled_data['scheduled_depart_time'],
+                    "actual_depart_time": crawled_data['actual_depart_time'],
+                    "destination": crawled_data['destination'],
+                    "airline": crawled_data['airline'],
+                    "terminal": crawled_data['terminal'],
+                    "gate": crawled_data['gate'],
+                    "status": crawled_data['status'],
+                    "time_difference": crawled_data['time_difference'],
+                    "updated_at": datetime.datetime.utcnow()
+                },
+                "$setOnInsert": {
+                    "created_at": datetime.datetime.utcnow()  # 只有在首次創建時 insert
+                }
+            },
+            upsert=True  # 如果沒有找到 match 'taiwan_title_time', 'airline'的 document，就 insert 一個新的
         )
-        check_file_exists(s3_client, bucket_name, key)
-        exists = check_file_exists(s3_client, bucket_name, key)
-        create_or_update_json(bucket_name, key, back_up_data, exists,s3_client)
+        logging.info(f"Matched count: {result.matched_count}")
+        logging.info(f"Modified count: {result.modified_count}")
+        if result.upserted_id:
+            logging.info(f'Upserted ID: {result.upserted_id}')  # 新建 document 的 ID
+    except Exception as e:
+        logging.error(f"An exception occurred: {str(e)}", exc_info=True)
+
+
+def back_up_to_s3(crawled_data):
+    load_dotenv()
+    bucket_name = os.getenv('BUCKET_NAME_flying_high')
+    aws_access_key = os.getenv('AWS_ACCESS_KEY')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    bucket_region = os.getenv('S3_BUCKET_REGION')
+    
+    # S3 client configuration
+    s3_client = boto3.client(
+        's3',
+        region_name=bucket_region,
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    
+    # folder name
+    taiwan_time_split = crawled_data['taiwan_title_time'].split("機")[1].split(" (")[0].strip().split("/")
+    taiwan_time_file = f"depart/{taiwan_time_split[0]}-{taiwan_time_split[1]}-{taiwan_time_split[2]}"
+    back_up_data = crawled_data
+    try:
+        airline_code = ""
+        for flight in crawled_data['airline']:
+            airline_code += flight['airline_code']
+            airline_code += "_"
+            key = f"{taiwan_time_file}_{airline_code}.json"
+            exists = check_file_exists(s3_client, bucket_name, key)
+            create_or_update_json(bucket_name, key, back_up_data, exists,s3_client)
     except Exception as e:
         logging.error(f"An exception occurred: {str(e)}", exc_info=True)
 
@@ -272,6 +274,7 @@ def check_file_exists(s3_client, bucket, key):
         return True
     except ClientError as e:
         return False
+    
     
 def create_or_update_json(bucket_name, key, data, exists, s3_client):
     if not exists:
@@ -350,7 +353,7 @@ with DAG(
     
     task_depart_flight = PythonOperator(
         task_id = "insert_depart_flight_mongodb",
-        python_callable=crawl_data,
+        python_callable=main_steps,
         dag = dag  
     )
     
