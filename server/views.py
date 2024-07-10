15bc1d056097e08202276d6ad6631b1163e437ab
Lily_Chou <98002855+hellowhaaa@users.noreply.github.com>
2024-07-10 16:56:44 +0800

    fix: can nott find depart_taiwan_datee key

--- server/views.py
+++ server/views.py
@@ -512,18 +512,21 @@ def my_insurance(current_user):
             user_flight = select_user_flight(current_user, logger=current_app.logger)
             return_flight_info = {}
             if user_flight is not None:
-                depart_taiwan_date = user_flight['depart_taiwan_date']
-                flight_depart_taoyuan = user_flight['flight_depart_taoyuan']
-                depart_flight = select_depart_flight_difference(depart_taiwan_date, flight_depart_taoyuan,logger=current_app.logger)
-                taiwan_tz = pytz.timezone('Asia/Taipei')
-                depart_taiwan_date_tz = depart_taiwan_date.replace(tzinfo=pytz.utc).astimezone(taiwan_tz)
-                formatted_date = depart_taiwan_date_tz.strftime('%Y-%m-%d')
-                return_flight_info = {
-                    "depart_flight":depart_flight,
-                    "flight_depart_taoyuan":flight_depart_taoyuan,
-                    "depart_taiwan_date":formatted_date
-                }
-                print("return_flight_info",return_flight_info)
+                if user_flight.get('depart_taiwan_date'):
+                    depart_taiwan_date = user_flight['depart_taiwan_date']
+                    flight_depart_taoyuan = user_flight['flight_depart_taoyuan']
+                    depart_flight = select_depart_flight_difference(depart_taiwan_date, flight_depart_taoyuan,logger=current_app.logger)
+                    taiwan_tz = pytz.timezone('Asia/Taipei')
+                    depart_taiwan_date_tz = depart_taiwan_date.replace(tzinfo=pytz.utc).astimezone(taiwan_tz)
+                    formatted_date = depart_taiwan_date_tz.strftime('%Y-%m-%d')
+                    return_flight_info = {
+                        "depart_flight":depart_flight,
+                        "flight_depart_taoyuan":flight_depart_taoyuan,
+                        "depart_taiwan_date":formatted_date
+                    }
+                else:
+                    return_flight_info = False
+                print("return_flight_info", return_flight_info)
             return render_template('my_insurance.html', user_insurance = user_insurance,insurance_content=insurance_content, return_flight_info = return_flight_info)
         else:
             return redirect(url_for('user_insurance'))
